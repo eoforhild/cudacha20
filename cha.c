278a1f579403de0ae314dc73999ef71f54fd9fd7
Peter <peaetchyousee@gmail.com>
2023-11-21 17:35:51 -0600

    faster

--- cha.c
+++ cha.c
@@ -20,14 +20,42 @@ const uint32_t CHACONST[4] = {0x61707865, 0x3320646e, 0x79622d32, 0x6b206574};
     arr[a] += arr[b]; arr[d] ^= arr[a]; arr[d] = leftRotate(arr[d], 8); \
     arr[c] += arr[d]; arr[b] ^= arr[c]; arr[b] = leftRotate(arr[b], 7);
 
+
+#define LOAD32_LE(SRC) load32_le(SRC)
+static inline uint32_t
+load32_le(const uint8_t src[4])
+{
+    uint32_t w = (uint32_t) src[0];
+    w |= (uint32_t) src[1] <<  8;
+    w |= (uint32_t) src[2] << 16;
+    w |= (uint32_t) src[3] << 24;
+    return w;
+}
+
 /**
  * Consumes current state and increment the counter
 */
-void chacha20_block(struct chacha_ctx* ctx) {
+static inline void chacha20_block(struct chacha_ctx* ctx) {
     uint32_t* keystream = ctx->keystream;
     uint32_t* state = ctx->state;
 
-    memcpy(keystream, state, 16 * sizeof(uint32_t));
+    keystream[0] = LOAD32_LE((uint8_t*)&state[0]);
+    keystream[1] = LOAD32_LE((uint8_t*)&state[1]);
+    keystream[2] = LOAD32_LE((uint8_t*)&state[2]);
+    keystream[3] = LOAD32_LE((uint8_t*)&state[3]);
+    keystream[4] = LOAD32_LE((uint8_t*)&state[4]);
+    keystream[5] = LOAD32_LE((uint8_t*)&state[5]);
+    keystream[6] = LOAD32_LE((uint8_t*)&state[6]);
+    keystream[7] = LOAD32_LE((uint8_t*)&state[7]);
+    keystream[8] = LOAD32_LE((uint8_t*)&state[8]);
+    keystream[9] = LOAD32_LE((uint8_t*)&state[9]);
+    keystream[10] = LOAD32_LE((uint8_t*)&state[10]);
+    keystream[11] = LOAD32_LE((uint8_t*)&state[11]);
+    keystream[12] = LOAD32_LE((uint8_t*)&state[12]);
+    keystream[13] = LOAD32_LE((uint8_t*)&state[13]);
+    keystream[14] = LOAD32_LE((uint8_t*)&state[14]);
+    keystream[15] = LOAD32_LE((uint8_t*)&state[15]);
+
     for (int i = 0; i < 10; i++) {
         QUARTERROUND(keystream, 0, 4, 8, 12);
         QUARTERROUND(keystream, 1, 5, 9, 13);
@@ -43,7 +71,7 @@ void chacha20_block(struct chacha_ctx* ctx) {
     *ctx->counter += 1;
 }
 
-void bytes_xor(char* result, int size, char* a, char* b) {
+static inline void bytes_xor(char* result, int size, char* a, char* b) {
     for (int i = 0; i < size; i++) {
         result[i] = a[i] ^ b[i];
     }
@@ -75,17 +103,28 @@ void file_xor(struct chacha_ctx* ctx, char* i, char* o) {
         exit(EXIT_FAILURE);
     }
     FILE* output = fopen(o, "w");
+    clock_t t;
+    double xor = 0.0;
+    double write = 0.0;
     do {
         len = fread((void*)buf, sizeof(char), CHUNKSIZE, input);
+        t = clock();
         chacha20_xor(ctx, buf, len);
+        t = clock() - t;
+        xor += ((double)t)/CLOCKS_PER_SEC;
+
+        t = clock();
         fwrite(buf, sizeof(char), len, output);
+        t = clock() - t;
+        write += ((double)t)/CLOCKS_PER_SEC;
     } while (len == CHUNKSIZE);
+    printf("The xor took %f seconds to execute\n", xor);
+    printf("The write took %f seconds to execute\n", write);
     fclose(input);
     fclose(output);
 }
 
 void init_chacha_ctx(struct chacha_ctx* ctx, uint32_t* key, uint32_t counter, uint32_t* nonce) {
-    memset(ctx, 0, sizeof(struct chacha_ctx));
     memcpy(ctx->state, CHACONST, 4 * sizeof(uint32_t));
     memcpy(ctx->state+4, key, 8 * sizeof(uint32_t));
     ctx->state[12] = counter;
